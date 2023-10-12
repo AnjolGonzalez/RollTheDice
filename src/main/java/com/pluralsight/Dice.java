069db9e6db3143f675f9bee59b430eae8e589317
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-10-12 13:55:57 -0400

    Completed

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,2 +1,15 @@
-package com.pluralsight;public class Dice {
-}
+package com.pluralsight;
+
+import java.util.Random;
+
+public class Dice {
+    int random;
+
+    public Dice(){
+        random = 0;
+    }
+    public int roll() {
+        int roll = 1 + (int) (Math.random() * 6);
+        return roll;
+    }
+    }
--- src/main/java/com/pluralsight/RollDice.java
+++ src/main/java/com/pluralsight/RollDice.java
@@ -1,7 +1,40 @@
 package com.pluralsight;
 
-public class roll {
-    public static void roll(String[] args) {
+public class RollDice {
+    public static void main(String[] args) {
+
+        Dice dice = new Dice();
+        int roll1;
+        int roll2;
+
+        int diceRoll2 = 0;
+        int diceRoll4 = 0;
+        int diceRoll6 = 0;
+        int diceRoll7 = 0;
+        int sum;
+
+        for (int i = 1; i <= 100; i++) ;
+        {
+            roll1 = dice.roll();
+            roll2 = dice.roll();
+            sum = roll1 + roll2;
+
+            if (sum == 2) {
+                diceRoll2++;
+            } else if (sum == 4) {
+                diceRoll4++;
+            } else if (sum == 6) {
+                diceRoll6++;
+            } else if (sum == 7) {
+                diceRoll7++;
+            }
+            System.out.print("Roll " + i + ": " + roll1 + " - " + roll2 + " Sum: " + sum + "\n");
+        }
+
+        System.out.println("Sum of two: " + diceRoll2);
+        System.out.println("Sum of four: " + diceRoll4);
+        System.out.println("Sum of six: " + diceRoll6);
+        System.out.println("Sum of seven: " + diceRoll7);
 
     }
 }
